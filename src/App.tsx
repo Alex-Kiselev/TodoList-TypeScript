9bb76c956d5d0bd0787d5728dad503b48f45c736
Alexxx <elbruswork1@gmail.com>
2022-03-29 14:42:20 +0300

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./App.css";
 
 const App: FC = () => {
+
+   const [task, setTask] = useState<string>('');
+   const [deadline, setDeadline] = useState<number>(0);
+   const [todo, setTodo] = useState([]);
+
+
+
+
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="вводите" />
+          <input  type="text" placeholder="вводите" />
           <input type="number" placeholder="вводите" />
         </div>
         <button>Жмяк</button>
